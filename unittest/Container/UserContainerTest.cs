17d8a9d5fba4e0f96b3fc2e33b1cec0e0ae25a11
Yordi Verschure <459372@student.fontys.nl>
2022-12-20 08:47:43 +0100

    start unittest

---
+++ unittest/Container/UserContainerTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic.Container;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Logic.Classes;
+using UnitTest.DALTest;
+using Interface.DTO;
+
+namespace UnitTest.Container
+{
+    [TestClass]
+    public class UserContainerTest
+    {
+        [TestMethod]
+        public void attemptLogin_Test()
+        {
+            UserContainer userContainer = new UserContainer(new user());
+            UserDTO userdto = new UserDTO() { Naam = "test", Wachtwoord = "123" };
+            User user = new User(userdto);
+            Assert.AreEqual(userContainer.attemptLogin(user), true);
+            
+        }
+    }
+}
---
+++ unittest/DALTest/UserTest.cs
@@ -0,0 +1,23 @@
+﻿using Interface.DTO;
+using Interface.Interface;
+using System.Collections.Generic;
+
+namespace UnitTest.DALTest
+{
+    public class user : IUserContext
+    {
+
+        public List<UserDTO> UserDTOs = new List<UserDTO>()
+        {
+            new UserDTO(){UserID=1, Naam = "test", Wachtwoord = "123"},
+            new UserDTO(){UserID=2, Naam = "TEST", Wachtwoord = "000"}
+        };
+
+
+
+        public UserDTO AttemptLogin(UserDTO userDTO)
+        {
+            return userDTO;
+        }
+    }
+}
